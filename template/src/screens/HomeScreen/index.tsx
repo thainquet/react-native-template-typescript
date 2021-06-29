4a033e7e762e86e04305510fa8a9802e5f1b647c
Thai Nguyen <thainq00@gmail.com>
2021-06-29 11:07:49 +0700

    init

---
+++ template/src/screens/HomeScreen/index.tsx
@@ -0,0 +1,26 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+import React from 'react';
+import { SafeAreaView, Text, Touchable, TouchableOpacity } from 'react-native';
+import { HomeStackParamsList } from '../../types/HomeParamsList';
+
+type HomeStackProps = StackNavigationProp<HomeStackParamsList, 'Home'>;
+
+type Props = {
+  navigation: HomeStackProps;
+};
+
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Setting');
+        }}>
+        <Text>Go Setting</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
